aa957106c524a3b70522ddbacef3471a3829a916
generaldigitalplatform <generaldigitalplatform@gmail.com>
2017-09-25 10:45:16 +0530

    'added'

--- api/config/database.js
+++ api/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'databaseUri':'mongodb://gdp-server:gdp-server@ds131512.mlab.com:31512/gdp-server?authMechanism=SCRAM-SHA-1',
 	'googlemapsapi':'https://maps.googleapis.com/maps/api/geocode/json?latlng=',
-	'job':'https://gdp-test-server.herokuapp.com/api/employee/job/',
+	'job':'https://gomunia-server.herokuapp.com/api/employee/job/',
 	'googlemapsapikey':'AIzaSyBh-Su_ZydI-kofZ0incHVCHPECZytqEK0'
 
 }
