54a34149853fa3c697d305c11d1ffebef8d580f5
generaldigitalplatform <generaldigitalplatform@gmail.com>
2017-10-27 11:55:47 +0530

    'added'

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(db.databaseUri,{useMongoClient:true});//,socketTimeoutMS:360000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'logs')));
+// app.use(express.static(path.join(__dirname, 'logs')));
 
 var employerRoute = require('./api/routes/employerRoute');
 var employeeRoute = require('./api/routes/employeeRoute');
@@ -32,10 +32,10 @@ var googleMapsRoute = require('./api/routes/googleMaps');
 var chatRoute = require('./api/routes/chatRoute');
 var chatGroupRoute = require('./api/routes/chatGroupRoute');
 
-var port = 3000;
+//var port = 3000;
 var httpServer = require('http').createServer(app);
-//httpServer.listen(port,ip, function() {
-httpServer.listen(port, function() {
+httpServer.listen(port,ip, function() {
+//httpServer.listen(port, function() {
     logger.info('gomunia-server started and running on port ' + port + ' and ip ' + ip);
 });
 
