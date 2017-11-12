9711525e5c61c23460c120ecc589f61521ece985
generaldigitalplatform <generaldigitalplatform@gmail.com>
2017-11-12 12:28:32 +0530

    'added'

--- api/config/database.js
+++ api/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
 	'databaseUri':'mongodb://gdp-server:gdp-server@ds131512.mlab.com:31512/gdp-server?authMechanism=SCRAM-SHA-1',
 	'googlemapsapi':'https://maps.googleapis.com/maps/api/geocode/json?latlng=',
 	'job':'https://gomunia-server.herokuapp.com/api/employee/job/',
-	'googlemapsapikey':'AIzaSyBh-Su_ZydI-kofZ0incHVCHPECZytqEK0',
+	'googlemapsapikey':'AIzaSyAIc3jm2K8HB447_a0AvfgW68QynsqncvM',	
 	'projectid':'gdsfieldforce',
 	'google_fcm_notification_url':'https://android.googleapis.com/gcm/notification',
 	'google_gdsfieldforce_server_key':'AAAAoOMA2jo:APA91bHneXgdTImlCiQY7YLaW7-PyT4eIS82r2aUknAW9EBQ9Y47vJeVSeekEG-QRVEVG-87FUuQisxhAEAKJPReKtD_xTxUpCcz6i1IouPiU49FHybkS62hzHJcf1uvb3ZDYoo4ZkgN',
